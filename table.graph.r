51b090faa3e54d6b259fd3c298298862558caa5e
David Ruau <davidruau@gmail.com>
2011-06-25 16:40:05 -0700

    table-graphic from Tufte

--- table.graph.r
+++ table.graph.r
@@ -13,12 +13,16 @@
 # Depending on the length of your rownames the margin might have to be adjusted
 #
 # df: data frame with 2 column
-# ...: supplementary arguments supply to par
+# line.col: vector length 2 with colors for the lines. Default to grey for
+#   value going up and black for value going down
+# label.cex: magnification for x and y labels
+# title.cex: magnificatoin for titles
+# ...: supplementary arguments supplied to par usually margin
 # table.graph(df, mar=c(5, 5, 1, 5))
 # 
 #####################################################
 
-table.graph <- function(df, line.col=c("grey", "black"), ...) {
+table.graph <- function(df, line.col=c("grey", "black"), label.cex=1, title.cex=1, ...) {
   xmin <- min(df)
   xmax <- max(df)
   X1 <- as.numeric(as.vector(df[,1]))
@@ -30,15 +34,15 @@ table.graph <- function(df, line.col=c("grey", "black"), ...) {
   # left
   plot(rep(0, nrow(df)), X1, xlim=c(0,1), ylim=c(xmin, xmax), 
     axes=FALSE, xlab='', ylab='', type='n')
-  mtext(text=paste(rownames(df), X1, sep='  '), side=2, at=X1, las=1, cex=0.7)
+  mtext(text=paste(rownames(df), X1, sep='  '), side=2, at=X1, las=1, cex=label.cex)
   par(new=TRUE)
   # right
   plot(rep(1, nrow(df)), X2, xlim=c(0,1), ylim=c(xmin, xmax), 
     axes=FALSE, xlab='', ylab='', type='n')
-  mtext(text=paste(X2, rownames(df), sep='  '), side=4, at=X2, las=1, cex=0.7)
+  mtext(text=paste(X2, rownames(df), sep='  '), side=4, at=X2, las=1, cex=label.cex)
   # class label
-  mtext(colnames(df)[1], side=3, at=0, cex=0.9)
-  mtext(colnames(df)[2], side=3, at=1, cex=0.9)
+  mtext(colnames(df)[1], side=3, at=0, cex=title.cex)
+  mtext(colnames(df)[2], side=3, at=1, cex=title.cex)
   # lines
   segments(x0 = rep(0, nrow(df)), y0 = X1, x1 = rep(1, nrow(df)), y1 = X2,
    col=ifelse({X1 - X2} < 0, line.col[1], line.col[2]))
