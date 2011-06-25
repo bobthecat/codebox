df355dcdbf0c4794eabc6c15bc3abce5d925d484
David Ruau <davidruau@gmail.com>
2011-06-25 16:31:41 -0700

    table graphic from Tufte

--- table.graph.r
+++ table.graph.r
@@ -13,17 +13,20 @@
 # Depending on the length of your rownames the margin might have to be adjusted
 #
 # df: data frame with 2 column
-# table.graph(df)
+# ...: supplementary arguments supply to par
+# table.graph(df, mar=c(5, 5, 1, 5))
 # 
 #####################################################
 
-table.graph <- function(df, line.col=c("grey", "black")) {
+table.graph <- function(df, line.col=c("grey", "black"), ...) {
   xmin <- min(df)
   xmax <- max(df)
-  X <- as.numeric(as.vector(x$[,1]))
-  X <- as.numeric(as.vector(x$[,2]))
-  # margin
-  par(mar=c(5, 8, 1, 8) + 0.1)
+  X1 <- as.numeric(as.vector(df[,1]))
+  X2 <- as.numeric(as.vector(df[,2]))
+  # original settings
+  old.par <- par(no.readonly = TRUE)
+  # par settings usually margins
+  par(...)
   # left
   plot(rep(0, nrow(df)), X1, xlim=c(0,1), ylim=c(xmin, xmax), 
     axes=FALSE, xlab='', ylab='', type='n')
@@ -33,10 +36,12 @@ table.graph <- function(df, line.col=c("grey", "black")) {
   plot(rep(1, nrow(df)), X2, xlim=c(0,1), ylim=c(xmin, xmax), 
     axes=FALSE, xlab='', ylab='', type='n')
   mtext(text=paste(X2, rownames(df), sep='  '), side=4, at=X2, las=1, cex=0.7)
-  # margin text
+  # class label
   mtext(colnames(df)[1], side=3, at=0, cex=0.9)
   mtext(colnames(df)[2], side=3, at=1, cex=0.9)
   # lines
   segments(x0 = rep(0, nrow(df)), y0 = X1, x1 = rep(1, nrow(df)), y1 = X2,
    col=ifelse({X1 - X2} < 0, line.col[1], line.col[2]))
+  # restore original settings
+  par(old.par)
 }
