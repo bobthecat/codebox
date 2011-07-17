e490f4edb04950e7267fbce9f6241d8f27681111
David Ruau <davidruau@gmail.com>
2011-07-16 22:19:16 -0700

    table.graph / slopegraph. Updated number rounding options.

--- table.graph.r
+++ table.graph.r
@@ -7,11 +7,11 @@
 # 
 #
 ##################### USAGE #########################
-# Tufte table-graphic shown in the Tufte book for paired numeric data
-# "the visal display of quantitative information" p. 158
+# Tufte table-graphic or slopegraph shown in the Tufte book for paired numeric data
+# "the visual display of quantitative information" p. 158
 #
 # Column names and row names will be used to label the plot
-# Depending on the length of your rownames the margin might have to be adjusted
+# Depending on the length of your rownames the margins might have to be adjusted
 #
 # df: data frame with 2 column
 # line.col: vector length 2 with colors for the lines. Default to grey for
@@ -19,14 +19,15 @@
 # label.cex: magnification for x and y labels from 0 to 1
 # title.cex: magnificatoin for titles from 0 to 1
 # digits: number of significant digits to report
-# ...: supplementary arguments supplied to par usually margin
+# ...: supplementary arguments supplied to par, usually margins
 # 
 # EXAMPLE:
-# table.graph(cars, label.cex=0.7, mar=c(5, 5, 1, 5))
+# source("table.graph.r")
+# table.graph(WorldPhones[,1:2], label.cex=0.7, title.cex=1.2, mar=c(5, 5, 1, 5))
 # 
 #####################################################
 
-table.graph <- function(df, line.col=c("grey", "black"), label.cex=1, title.cex=1, width = 6, digits = 3, rounding.method = "round", ...) {
+table.graph <- function(df, line.col=c("grey", "black"), label.cex=1, title.cex=1, width = 6, digits = 2, rounding.method = NULL, ...) {
   xmin <- min(df)
   xmax <- max(df)
   X1 <- as.numeric(as.vector(df[,1]))
@@ -36,9 +37,14 @@ table.graph <- function(df, line.col=c("grey", "black"), label.cex=1, title.cex=
   # par settings usually margins
   par(...)
   # rounding
-  rounding.character <- switch(match(rounding.method, c("round", "signif")), "f", "g")
-  if(is.null(rounding.character)) stop("rounding method must be either \"round\" or \"signif\"")
-  fmt = paste("%", width, ".", digits, rounding.character, sep = "")
+  if(is.null(rounding.method)){
+    fmt = "%s"
+  } 
+  else{
+    rounding.character <- switch(match(rounding.method, c("round", "signif")), "f", "g")
+    fmt = paste("%", width, ".", digits, rounding.character, sep = "")
+  }
+  
   # left
   plot(rep(0, nrow(df)), X1, xlim=c(0,1), ylim=c(xmin, xmax), 
     axes=FALSE, xlab='', ylab='', type='n')
