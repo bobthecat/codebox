273b70574b3a34552fe1381461f083b705beb0ca
David Ruau <davidruau@gmail.com>
2011-07-18 23:23:47 -0700

    slopegraph. improve numerical value display

--- slopegraph.r
+++ slopegraph.r
@@ -16,33 +16,38 @@
 # EXAMPLE:
 # source('slopegraph.r')
 # pdf('slopegraph.pdf', height=7, width=8)
-# slopegraph(data = WorldPhones[,1:3], mymain = "YEARS", mar=c(5, 5, 5, 5))
+# slopegraph(data = t(WorldPhones[,1:3]), mymain = "YEARS", mar=c(2, 8, 5, 8))
 # dev.off()
+#
+# Tips: when value overlap first extend the height of you plot
+# second round your value
 # 
 #####################################################
 
 slopegraph <- function(data, label.cex=0.9, axis.cex=0.9, digits = 2, rounding.method = NULL, mymain = "slopegraph", ...) {
   require(plotrix)
+  if(!is.data.frame(data)){
+    data <- as.data.frame(data)
+  }
+  
   if(!is.null(rounding.method)){
-    fmt <- .rd.method(rounding.method, width, digits)
-    data.annot <- sprintf(fmt, data)
+    data.temp <- .rd.method(rounding.method, width, digits)
+    data.temp <- as.numeric(sprintf(fmt, as.matrix(data)))
+    data <- as.data.frame(matrix(data.temp, nrow=nrow(data), ncol=ncol(data), dimnames=list(rownames(data), colnames(data))))
   }
-  else{
-    data.annot <- as.character(as.vector(data))
-  }
-  data.annot <- matrix(data.annot, nrow=nrow(data), ncol=ncol(data))
+  
   old.par <- par(no.readonly = TRUE)
   par(...)
-  matplot(data, type='b', pch=NA, axes=FALSE, xlab='', ylab='', lty='solid', col="grey", ...)
-  mtext(text = rownames(data), side = 3, at=1:nrow(data), line = 1, cex=axis.cex)
-  mtext(text = colnames(data), side = 2, at=data[1,], line = 1, las=1, cex=axis.cex)
+  matplot(t(data), type='b', pch=NA, axes=FALSE, xlab='', ylab='', lty='solid', col="grey", ...)
+  mtext(text = colnames(data), side = 3, at=1:ncol(data), line = 1, cex=axis.cex)
+  mtext(text = rownames(data), side = 2, at=data[,1], line = 0.5, las=1, cex=axis.cex)
   title(main = mymain, line=3)
-  for(i in 1:nrow(data)){
-    for(j in 1:ncol(data)){
-      boxed.labels(i, data[i,j], labels=data.annot[i,j], bg='white', border = FALSE, cex=label.cex)
+  for(i in 1:ncol(data)){
+    for(j in 1:nrow(data)){
+      boxed.labels(i, data[j,i], labels=data[j,i], bg='white', border = FALSE, cex=label.cex)
     }
   }
-  mtext(text = colnames(data), side = 4, at=data[nrow(data),], line = 1, las=1, cex=axis.cex)
+  mtext(text = rownames(data), side = 4, at=data[,ncol(data)], line = 0.5, las=1, cex=axis.cex)
   par(old.par)
 }
 
