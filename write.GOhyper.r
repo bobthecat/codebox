44ee0d36a9ff14786c7c891781a07f676ac12631
David Ruau <davidruau@gmail.com>
2012-02-14 12:17:33 -0800

    initial commit

---
+++ write.GOhyper.r
@@ -0,0 +1,24 @@
+#
+# write.GOhyper.r
+#
+# Created by David Ruau on 2012-01-23.  
+# Dept. of Pediatrics/Div. Systems Medicine, Stanford University.
+# 
+#
+##################### USAGE #########################
+# 
+# write.GOhyper(mfhyper, filename="results.xlsx")
+# 
+#####################################################
+
+write.GOhyper <- function(mfhyper, filename='GO_results.xlsx') {
+  require(GOstats)
+  require(multtest)
+  require(xlsx)
+  gogo <- summary(mfhyper)
+  gogo$adjPvalue <- mt.rawp2adjp(gogo$Pvalue)$adjp[,"BH"]
+  gogo <- gogo[,c(1:2,8,3:7)]
+  write.xlsx(gogo, file=filename, sep=',', row.names = FALSE)
+  print(paste('Results written in', filename))
+  return(gogo)
+}
