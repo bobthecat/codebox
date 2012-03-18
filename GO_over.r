bd650873e7f992129978cdbaa915ae9da760da83
David Ruau <davidruau@gmail.com>
2012-03-18 16:30:08 -0700

    Revert "Updated usage description"
    This reverts commit c1989c242ed4e4a870ca486b36129131d95f09b5.

--- GO_over.r
+++ GO_over.r
@@ -7,9 +7,7 @@
 #
 ##################### USAGE #########################
 # 
-# mfhyper <- GO_over(entrezUniverse, glist=glist, annot='org.Hs.eg.db')
-# geneMappedCount(mfhyper)
-# htmlReport(mfhyper, file="results.html", label="ceci n'est pas une pipe")
+# GO_over(...)
 # 
 #####################################################
 
