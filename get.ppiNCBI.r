4a2a348a716a9f6dca49db083b06dd48554762c2
David Ruau <davidruau@gmail.com>
2012-06-05 10:25:35 -0700

    Obtain protein-protein interaction for a gene list

---
+++ get.ppiNCBI.r
@@ -0,0 +1,36 @@
+#
+# get.ppiNCBI.r
+#
+# Created by David Ruau on 2012-06-05.
+# Dept. of Pediatrics/Div. Systems Medicine, Stanford University.
+# 
+#
+##################### USAGE #########################
+# see http://brainchronicle.blogspot.com/2012/05/obtain-protein-protein-interaction-from.html
+# ppi <- get.ppiNCBI(c("777", "57619"))
+# 
+#####################################################
+
+get.ppiNCBI <- function(g.n) {
+  require(XML)
+  ppi <- data.frame()
+  for(i in 1:length(g.n)){
+    o <- htmlParse(paste("http://www.ncbi.nlm.nih.gov/gene/", g.n[i], sep=''))
+    # check if interaction table exists
+    exist <- length(getNodeSet(o, "//table//th[@id='inter-prod']"))>0
+    if(exist){
+      p <- getNodeSet(o, "//table")
+      int <- readHTMLTable(p[[1]])
+      ppi <- rbind(ppi, data.frame(egID=g.n[i], intSymbol=int$`Other Gene`))
+    }
+    # play nice! and avoid being kicked out from NCBI servers
+    Sys.sleep(1)
+  }
+  if(dim(ppi)[1]>0){
+    ppi <- unique(ppi)
+    print(paste(dim(ppi)[1], "interactions found"))
+    return(ppi)
+  } else{
+    print("No interaction found")
+  }
+}
